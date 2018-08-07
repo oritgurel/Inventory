7df89a758663d3e32d579d8f04b40c221d34a6e4
OGIT <oritgurel@gmail.com>
2018-08-07 23:42:44 +0300

    initial commit

--- app/src/main/java/com/oritmalki/inventory/products/data/ProductsDbHelper.java
+++ app/src/main/java/com/oritmalki/inventory/products/data/ProductsDbHelper.java
@@ -6,16 +6,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class ProductsDbHelper extends SQLiteOpenHelper {
 
+    public static final String LOG_TAG = ProductsDbHelper.class.getSimpleName();
+    private static final String DATABASE_NAME = "inventory.db";
+    private static final int DATABASE_VERSION = 1;
 
-
-
-    public ProductsDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public ProductsDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_PRODUCTS_TABLE =  "CREATE TABLE " + ProductsContract.ProductEntry.TABLE_NAME + " ("
+                + ProductsContract.ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProductsContract.ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + ProductsContract.ProductEntry.COLUMN_PRODUCT_PRICE + " FLOAT, "
+                + ProductsContract.ProductEntry.COLUMN_PRODUCT_QUANTITY + " INTEGER NOT NULL DEFAULT 0, "
+                + ProductsContract.ProductEntry.COLUMN_SUPPLIER_NAME + " INTEGER NOT NULL DEFAULT 0, "
+                + ProductsContract.ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER + " TEXT);";
 
+        // Execute the SQL statement
+        sqLiteDatabase.execSQL(SQL_CREATE_PRODUCTS_TABLE);
     }
 
     @Override
