3bf4c9de9cd2dff26f5fc5450d3c5a18fe3dc428
OGIT <oritgurel@gmail.com>
2018-08-08 21:16:23 +0300

    initial commit

--- app/src/main/java/com/oritmalki/inventory/products/data/ProductProvider.java
+++ app/src/main/java/com/oritmalki/inventory/products/data/ProductProvider.java
@@ -1,36 +1,107 @@
 package com.oritmalki.inventory.products.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 public class ProductProvider extends ContentProvider {
 
+    public static final String LOG_TAG = ProductProvider.class.getSimpleName();
+    private static final int PRODUCTS = 100;
+    private static final int PRODUCT_ID = 101;
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        sUriMatcher.addURI(ProductsContract.CONTENT_AUTHORITY, ProductsContract.PATH_PRODUCTS, PRODUCTS);
+        sUriMatcher.addURI(ProductsContract.CONTENT_AUTHORITY, ProductsContract.PATH_PRODUCTS + "/#", PRODUCT_ID);
+    }
+    private static final String PHONE_REGEX = "^[0][1-9]\\d{8}$|^[1-9]\\d{8}$";
+
+    private ProductsDbHelper mDbHelper;
 
 
     @Override
     public boolean onCreate() {
-        return false;
+        mDbHelper = new ProductsDbHelper(getContext());
+        return true;
     }
 
     @Nullable
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public String getType(@NonNull Uri uri) {
-        return null;
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+        Cursor cursor;
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                cursor = database.query(ProductsContract.ProductEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case PRODUCT_ID:
+                selection = ProductsContract.ProductEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                cursor = database.query(ProductsContract.ProductEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    private Uri insertProduct(Uri uri, ContentValues values) {
+
+        String name = values.getAsString(ProductsContract.ProductEntry.COLUMN_PRODUCT_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Product requires a name");
+        }
+
+        Integer supplierName = values.getAsInteger(ProductsContract.ProductEntry.COLUMN_SUPPLIER_NAME);
+        if (supplierName == null || !ProductsContract.ProductEntry.isValidSupplierCode(supplierName)) {
+            throw new IllegalArgumentException("Product requires valid supplier name");
+        }
+
+        Integer quantity = values.getAsInteger(ProductsContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
+        if (quantity != null && quantity < 0) {
+            throw new IllegalArgumentException("Product requires valid quantity");
+        }
+
+        Float price = values.getAsFloat(ProductsContract.ProductEntry.COLUMN_PRODUCT_PRICE);
+        if (price != null && price < 0) {
+            throw new IllegalArgumentException("Product requires valid price");
+        }
+
+        String supplierPhoneNumber = values.getAsString(ProductsContract.ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER);
+
+
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+        long id = database.insert(ProductsContract.ProductEntry.TABLE_NAME, null, values);
+        // If the ID is -1, then the insertion failed. Log an error and return null.
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return ContentUris.withAppendedId(uri, id);
+    }
+
+
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
         return null;
     }
 
