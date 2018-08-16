a71310ed0355c985c944642d78985b68b7491856
oritmalki <oritgurel@gmail.com>
2018-08-17 00:25:14 +0300

    cart button

--- app/src/main/java/com/oritmalki/inventory/products/ProductsCursorAdapter.java
+++ app/src/main/java/com/oritmalki/inventory/products/ProductsCursorAdapter.java
@@ -5,9 +5,12 @@ import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.oritmalki.inventory.products.data.ProductProvider;
 import com.oritmalki.inventory.products.data.ProductsContract;
 
 public class ProductsCursorAdapter extends CursorAdapter {
@@ -22,27 +25,32 @@ public class ProductsCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(final View view, final Context context, final Cursor cursor) {
 
         TextView nameTv = view.findViewById(R.id.product_list_item_name_tv);
         TextView priceTv = view.findViewById(R.id.product_list_item_price_tv);
         TextView quantityTv = view.findViewById(R.id.product_list_item_quantity_tv);
-        TextView supplierNameTv = view.findViewById(R.id.product_list_item_supplier_tv);
+        ImageButton saleBtn = view.findViewById(R.id.product_list_sale_btn);
+
 
         int nameColumnIndex = cursor.getColumnIndex(ProductsContract.ProductEntry.COLUMN_PRODUCT_NAME);
         int priceColumnIndex = cursor.getColumnIndex(ProductsContract.ProductEntry.COLUMN_PRODUCT_PRICE);
-        int quantityColumnIndex = cursor.getColumnIndex(ProductsContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
-        int supplierNmaeColumnIndex = cursor.getColumnIndex(ProductsContract.ProductEntry.COLUMN_SUPPLIER_NAME);
+        final int quantityColumnIndex = cursor.getColumnIndex(ProductsContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
 
         String name = cursor.getString(nameColumnIndex);
         Float price = cursor.getFloat(priceColumnIndex);
-        int quantity = cursor.getInt(quantityColumnIndex);
-        String supplierName = cursor.getString(supplierNmaeColumnIndex);
+        final int quantity = cursor.getInt(quantityColumnIndex);
 
         nameTv.setText(name);
         priceTv.setText(String.valueOf(price));
         quantityTv.setText(String.valueOf(quantity));
-        supplierNameTv.setText(supplierName);
+        saleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                long id = ((Long) view.getTag());
+//                context.getContentResolver().delete(ProductsContract.ProductEntry.CONTENT_URI, quantityColumnIndex,)
+            }
+        });
 
     }
 }
